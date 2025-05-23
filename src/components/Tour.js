be939570c9f8ed24b543a9e518c58ce65ee83300
Tushar Ahuja <tusharahuja668@gmail.com>
2025-05-23 11:02:34 +0530

    change in repo

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
+
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -10,13 +11,17 @@ const Tour = ({ id, image, info, price, name, removeTour }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>
+        <p id={`tour-item-para-${id}`}>
           {readMore ? info : `${info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)}>
             {readMore ? 'Show less' : 'Read more'}
           </button>
         </p>
-        <button className="delete-btn" onClick={() => removeTour(id)}>
+        <button
+          id={`delete-btn-${id}`}
+          className="delete-btn"
+          onClick={() => removeTour(id)}
+        >
           Not Interested
         </button>
       </footer>
